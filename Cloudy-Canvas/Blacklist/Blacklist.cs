947c415c2d173cdbf36665a46d0359673d7f7d1e
Raymond Welch <welch.raymond@gmail.com>
2021-04-06 19:35:26 -0500

    Added blacklist

---
+++ Cloudy-Canvas/Blacklist/Blacklist.cs
@@ -0,0 +1,72 @@
+﻿namespace Cloudy_Canvas.Blacklist
+{
+    using System.Collections.Generic;
+
+    public class Blacklist
+    {
+        private List<string> BlackList;
+
+        public Blacklist()
+        {
+            BlackList = new List<string>();
+            InitializeList();
+        }
+
+        private void InitializeList()
+        {
+            BlackList.Add("aryanne");
+        }
+
+        public bool AddTerm(string term)
+        {
+            var lower = term.ToLower();
+            if (BlackList.Contains(lower))
+            {
+                return false;
+            }
+            BlackList.Add(lower);
+            return true;
+
+        }
+
+        public bool RemoveTerm(string term)
+        {
+            var lower = term.ToLower();
+            if (!BlackList.Contains(lower))
+            {
+                return false;
+            }
+            BlackList.Remove(lower);
+            return true;
+
+        }
+
+        public List<string> GetList()
+        {
+            return BlackList;
+        }
+        public string CheckList(string query)
+        {
+            var lower = query.ToLower();
+            var matchedTerms = "";
+            foreach (var term in BlackList)
+            {
+                if (!lower.Contains(term))
+                {
+                    return matchedTerms;
+                }
+
+                if (matchedTerms == "")
+                {
+                    matchedTerms += term;
+                }
+                else
+                {
+                    matchedTerms += $", {term}";
+                }
+            }
+
+            return matchedTerms;
+        }
+    }
+}
--- Cloudy-Canvas/Modules/BooruModule.cs
+++ Cloudy-Canvas/Modules/BooruModule.cs
@@ -1,6 +1,7 @@
 ﻿namespace Cloudy_Canvas.Modules
 {
     using System.Threading.Tasks;
+    using Cloudy_Canvas.Blacklist;
     using Cloudy_Canvas.Service;
     using Discord.Commands;
     using Microsoft.Extensions.Logging;
@@ -9,26 +10,37 @@
     {
         private readonly BooruService _booru;
         private readonly ILogger<Worker> _logger;
+        private readonly Blacklist _blacklist;
 
-        public BooruModule(ILogger<Worker> logger, BooruService booru)
+        public BooruModule(ILogger<Worker> logger, BooruService booru, Blacklist blacklist)
         {
             _logger = logger;
             _booru = booru;
+            _blacklist = blacklist;
         }
 
         [Command("pick")]
         [Summary("Selects an image at random")]
         public async Task PickAsync([Remainder] [Summary("Query string")] string query = "*")
         {
-            var id = await _booru.GetRandomFirstPageImageByQuery(query);
-            _logger.LogInformation($"query: {query}, result: {id}");
-            if (id == -1)
+            var badTerms = _blacklist.CheckList(query);
+            if (badTerms != "")
             {
-                await ReplyAsync("I could not find any images with that query.");
+                _logger.LogInformation($"query: BLACKLISTED {badTerms}");
+                await ReplyAsync("I'm not gonna go look for that.");
             }
             else
             {
-                await ReplyAsync($"https://manebooru.art/images/{id}");
+                var id = await _booru.GetRandomFirstPageImageByQuery(query);
+                _logger.LogInformation($"query: {query}, result: {id}");
+                if (id == -1)
+                {
+                    await ReplyAsync("I could not find any images with that query.");
+                }
+                else
+                {
+                    await ReplyAsync($"https://manebooru.art/images/{id}");
+                }
             }
         }
 
@@ -36,15 +48,24 @@
         [Summary("Selects an image by image id")]
         public async Task IdAsync([Summary("The image ID")] long id = 4010266)
         {
-            var result = await _booru.GetImageById(id);
-            _logger.LogInformation($"id: {id}, {result}");
-            if (result == -1)
+            var badTerms = _blacklist.CheckList(id.ToString());
+            if (badTerms != "")
             {
-                await ReplyAsync("I could not find that image.");
+                _logger.LogInformation($"id: BLACKLISTED {badTerms}");
+                await ReplyAsync("I'm not gonna go look for that.");
             }
             else
             {
-                await ReplyAsync($"https://manebooru.art/images/{id}");
+                var result = await _booru.GetImageById(id);
+                _logger.LogInformation($"id: {id}, {result}");
+                if (result == -1)
+                {
+                    await ReplyAsync("I could not find that image.");
+                }
+                else
+                {
+                    await ReplyAsync($"https://manebooru.art/images/{id}");
+                }
             }
         }
     }
