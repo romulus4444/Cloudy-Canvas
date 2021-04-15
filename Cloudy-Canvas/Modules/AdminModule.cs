5fac19debac52a27069482cde0e96bbdb75e4cb9
Raymond Welch <welch.raymond@gmail.com>
2021-04-15 00:09:18 -0500

    Tidied up the nested switches

--- Cloudy-Canvas/Modules/AdminModule.cs
+++ Cloudy-Canvas/Modules/AdminModule.cs
@@ -20,39 +20,20 @@
                     await ReplyAsync("You need to specify an admin command.");
                     break;
                 case "adminchannel":
-                    if (commandTwo == "")
+                    switch (commandTwo)
                     {
-                        var channelId = await GetAdminChannelAsync(Context);
-                        if (channelId > 0)
-                        {
-                            await ReplyAsync($"Admin channel is <#{channelId}>");
-                        }
-                        else
-                        {
-                            await ReplyAsync("Admin channel not set yet.");
-                        }
-                    }
-                    else
-                    {
-                        var channelId = await DiscordHelper.GetChannelIdIfAccessAsync(commandTwo, Context);
-                        var channelName = await DiscordHelper.ConvertChannelPingToNameAsync(commandTwo, Context);
-                        if (channelId > 0)
-                        {
-                            if (channelName.Contains("<ERROR>"))
-                            {
-                                await SetAdminChannelAsync(channelId, commandTwo);
-                            }
-                            else
-                            {
-                                await SetAdminChannelAsync(channelId, channelName);
-                            }
-
-                            await ReplyAsync($"Admin channel set to <#{channelId}>");
-                        }
-                        else
-                        {
-                            await ReplyAsync($"Invalid channel name #{commandTwo}.");
-                        }
+                        case "":
+                            await ReplyAsync("You must specify a subcommand.");
+                            break;
+                        case "get":
+                            await AdminChannelGetAsync();
+                            break;
+                        case "set":
+                            await AdminChannelSetAsync(commandThree);
+                            break;
+                        default:
+                            await ReplyAsync($"Invalid command {commandTwo}");
+                            break;
                     }
 
                     break;
@@ -63,77 +44,16 @@
                             await ReplyAsync("You must specify a subcommand.");
                             break;
                         case "get":
-                            var channelList = await GetIgnoredChannelsAsync(Context);
-                            if (channelList.Count > 0)
-                            {
-                                var output = "__Channel Ignore List:__\n";
-                                foreach (var channel in channelList)
-                                {
-                                    output += $"<#{channel}>\n";
-                                }
-
-                                await ReplyAsync(output);
-                            }
-                            else
-                            {
-                                await ReplyAsync("No channels on ignore list.");
-                            }
-
+                            await IgnoreChannelGetAsync();
                             break;
                         case "add":
-                            var channelAddId = await DiscordHelper.GetChannelIdIfAccessAsync(commandThree, Context);
-                            var channelAddName = await DiscordHelper.ConvertChannelPingToNameAsync(commandThree, Context);
-                            if (channelAddId > 0)
-                            {
-                                bool added;
-                                if (channelAddName.Contains("<ERROR>"))
-                                {
-                                    added = await AddIgnoreChannelAsync(channelAddId, commandThree, Context);
-                                }
-                                else
-                                {
-                                    added = await AddIgnoreChannelAsync(channelAddId, channelAddName, Context);
-                                }
-
-                                if (added)
-                                {
-                                    await ReplyAsync($"Added <#{channelAddId}> to ignore list.");
-                                }
-                                else
-                                {
-                                    await ReplyAsync($"<#{channelAddId}> is already on the list.");
-                                }
-                            }
-                            else
-                            {
-                                await ReplyAsync($"Invalid channel name #{commandThree}.");
-                            }
-
+                            await IgnoreChannelAddAsync(commandThree);
                             break;
                         case "remove":
-                            var channelRemoveId = await DiscordHelper.GetChannelIdIfAccessAsync(commandThree, Context);
-                            if (channelRemoveId > 0)
-                            {
-                                var removed = await RemoveIgnoreChannelAsync(channelRemoveId, Context);
-
-                                if (removed)
-                                {
-                                    await ReplyAsync($"Removed <#{channelRemoveId}> from ignore list.");
-                                }
-                                else
-                                {
-                                    await ReplyAsync($"<#{channelRemoveId}> was not on the list.");
-                                }
-                            }
-                            else
-                            {
-                                await ReplyAsync($"Invalid channel name #{commandThree}.");
-                            }
-
+                            await IgnoreChannelRemoveAsync(commandThree);
                             break;
                         case "clear":
-                            await ClearIgnoreChannelAsync(Context);
-                            await ReplyAsync("Ignore channel list cleared.");
+                            await IgnoreChannelClearAsync();
                             break;
                         default:
                             await ReplyAsync($"Invalid command {commandTwo}");
@@ -233,10 +153,125 @@
 
             return removed;
         }
-        private static async Task ClearIgnoreChannelAsync( SocketCommandContext context)
+
+        private static async Task ClearIgnoreChannelAsync(SocketCommandContext context)
         {
             var filename = FileHelper.SetUpFilepath(FilePathType.Server, "IgnoredChannels", "txt", context);
             await File.WriteAllTextAsync(filename, "");
+        }
+
+        private async Task AdminChannelSetAsync(string commandThree)
+        {
+            var channelSetId = await DiscordHelper.GetChannelIdIfAccessAsync(commandThree, Context);
+            var channelSetName = await DiscordHelper.ConvertChannelPingToNameAsync(commandThree, Context);
+            if (channelSetId > 0)
+            {
+                if (channelSetName.Contains("<ERROR>"))
+                {
+                    await SetAdminChannelAsync(channelSetId, commandThree);
+                }
+                else
+                {
+                    await SetAdminChannelAsync(channelSetId, channelSetName);
+                }
+
+                await ReplyAsync($"Admin channel set to <#{channelSetId}>");
+            }
+            else
+            {
+                await ReplyAsync($"Invalid channel name #{commandThree}.");
+            }
+        }
+
+        private async Task AdminChannelGetAsync()
+        {
+            var channelGetId = await GetAdminChannelAsync(Context);
+            if (channelGetId > 0)
+            {
+                await ReplyAsync($"Admin channel is <#{channelGetId}>");
+            }
+            else
+            {
+                await ReplyAsync("Admin channel not set yet.");
+            }
+        }
+
+        private async Task IgnoreChannelGetAsync()
+        {
+            var channelList = await GetIgnoredChannelsAsync(Context);
+            if (channelList.Count > 0)
+            {
+                var output = "__Channel Ignore List:__\n";
+                foreach (var channel in channelList)
+                {
+                    output += $"<#{channel}>\n";
+                }
+
+                await ReplyAsync(output);
+            }
+            else
+            {
+                await ReplyAsync("No channels on ignore list.");
+            }
+        }
+
+        private async Task IgnoreChannelClearAsync()
+        {
+            await ClearIgnoreChannelAsync(Context);
+            await ReplyAsync("Ignore channel list cleared.");
+        }
+
+        private async Task IgnoreChannelRemoveAsync(string commandThree)
+        {
+            var channelRemoveId = await DiscordHelper.GetChannelIdIfAccessAsync(commandThree, Context);
+            if (channelRemoveId > 0)
+            {
+                var removed = await RemoveIgnoreChannelAsync(channelRemoveId, Context);
+
+                if (removed)
+                {
+                    await ReplyAsync($"Removed <#{channelRemoveId}> from ignore list.");
+                }
+                else
+                {
+                    await ReplyAsync($"<#{channelRemoveId}> was not on the list.");
+                }
+            }
+            else
+            {
+                await ReplyAsync($"Invalid channel name #{commandThree}.");
+            }
+        }
+
+        private async Task IgnoreChannelAddAsync(string commandThree)
+        {
+            var channelAddId = await DiscordHelper.GetChannelIdIfAccessAsync(commandThree, Context);
+            var channelAddName = await DiscordHelper.ConvertChannelPingToNameAsync(commandThree, Context);
+            if (channelAddId > 0)
+            {
+                bool added;
+                if (channelAddName.Contains("<ERROR>"))
+                {
+                    added = await AddIgnoreChannelAsync(channelAddId, commandThree, Context);
+                }
+                else
+                {
+                    added = await AddIgnoreChannelAsync(channelAddId, channelAddName, Context);
+                }
+
+                if (added)
+                {
+                    await ReplyAsync($"Added <#{channelAddId}> to ignore list.");
+                }
+                else
+                {
+                    await ReplyAsync($"<#{channelAddId}> is already on the list.");
+                }
+            }
+            else
+            {
+                await ReplyAsync($"Invalid channel name #{commandThree}.");
+            }
         }
 
         private async Task SetAdminChannelAsync(ulong channelId, string channelName)
