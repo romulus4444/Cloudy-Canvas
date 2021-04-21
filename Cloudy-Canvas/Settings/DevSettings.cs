ca53e08c5499cdca72b6e52b52f87cf772033369
Raymond Welch <welch.raymond@gmail.com>
2021-04-20 21:56:59 -0500

    Switch to dev

--- Cloudy-Canvas/Settings/DevSettings.cs
+++ Cloudy-Canvas/Settings/DevSettings.cs
@@ -2,16 +2,16 @@
 {
     public static class DevSettings
     {
-        //-------------------------Production-------------------------
-        public static char prefix { get; } = ';'; //production prefix
-        public static ulong CloudyCanvasId { get; } = 828682017868218445; //Cloudy Canvas's Production Id
-        public static string RootPath { get; } = "BotSettings"; //Production folders
+        ////-------------------------Production-------------------------
+        //public static char prefix { get; } = ';'; //production prefix
+        //public static ulong CloudyCanvasId { get; } = 828682017868218445; //Cloudy Canvas's Production Id
+        //public static string RootPath { get; } = "BotSettings"; //Production folders
         //------------------------------------------------------------
 
-        ////-------------------------Development------------------------
-        //public static char prefix { get; } = '?'; //development prefix
-        //public static ulong CloudyCanvasId { get; } = 833556029521657856; //Cloudy Canvas's Development Id
-        //public static string RootPath { get; } = "DevSettings"; //Development folders
-        ////------------------------------------------------------------
+        //-------------------------Development------------------------
+        public static char prefix { get; } = '?'; //development prefix
+        public static ulong CloudyCanvasId { get; } = 833556029521657856; //Cloudy Canvas's Development Id
+        public static string RootPath { get; } = "DevSettings"; //Development folders
+        //------------------------------------------------------------
     }
 }
