5e2dcf6d56ac9dbaddd2750f057c2ab52bb9f605
Raymond Welch <welch.raymond@gmail.com>
2021-04-21 19:39:24 -0500

    Adminrole immune to ignorechannel and ignorerole - hide badlist debug output

--- Cloudy-Canvas/Modules/BooruModule.cs
+++ Cloudy-Canvas/Modules/BooruModule.cs
@@ -305,19 +305,19 @@
         private async Task<bool> CheckBadlists(string query)
         {
             await BadlistHelper.InitializeYellowList(Context);
-            await _logger.Log("<DEBUG> Checking for yellow terms", Context);
+            //await _logger.Log("<DEBUG> Checking for yellow terms", Context);
             var yellowTerms = await BadlistHelper.CheckYellowList(query, Context);
-            foreach (var yellowTerm in yellowTerms)
-            {
-                await _logger.Log(yellowTerm, Context);
-            }
+            //foreach (var yellowTerm in yellowTerms)
+            //{
+            //    await _logger.Log(yellowTerm, Context);
+            //}
 
-            await _logger.Log("<DEBUG> Checking for red terms", Context);
+            //await _logger.Log("<DEBUG> Checking for red terms", Context);
             var redTerms = await BadlistHelper.CheckRedList(query, Context);
-            foreach (var redTerm in redTerms)
-            {
-                await _logger.Log(redTerm, Context);
-            }
+            //foreach (var redTerm in redTerms)
+            //{
+            //    await _logger.Log(redTerm, Context);
+            //}
             if (redTerms[0] != "")
             {
                 await _logger.Log($"pick: {query}, REDLISTED {redTerms[0]}", Context, true);
