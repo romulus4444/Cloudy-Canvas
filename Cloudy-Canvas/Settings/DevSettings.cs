aa391fd4630eeb14065496416162cbf353cfee9d
Raymond Welch <welch.raymond@gmail.com>
2021-04-27 17:15:46 -0500

    switch to prod

--- Cloudy-Canvas/Settings/DevSettings.cs
+++ Cloudy-Canvas/Settings/DevSettings.cs
@@ -2,16 +2,16 @@
 {
     public static class DevSettings
     {
-        ////-------------------------Production-------------------------
-        //public static char prefix { get; } = ';'; //production prefix
-        //public static bool useDevPrefix { get; } = false; //Use the alternate prefix
-        //public static string RootPath { get; } = "botsettings"; //Production folders
-        ////------------------------------------------------------------
-
-        //-------------------------Development------------------------
-        public static char prefix { get; } = '?'; //development prefix
-        public static bool useDevPrefix { get; } = true; //Use the alternate prefix
-        public static string RootPath { get; } = "devsettings"; //Development folders
+        //-------------------------Production-------------------------
+        public static char prefix { get; } = ';'; //production prefix
+        public static bool useDevPrefix { get; } = false; //Use the alternate prefix
+        public static string RootPath { get; } = "botsettings"; //Production folders
         //------------------------------------------------------------
+
+        ////-------------------------Development------------------------
+        //public static char prefix { get; } = '?'; //development prefix
+        //public static bool useDevPrefix { get; } = true; //Use the alternate prefix
+        //public static string RootPath { get; } = "devsettings"; //Development folders
+        ////------------------------------------------------------------
     }
 }
